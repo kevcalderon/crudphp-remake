da0ea78050ad6c8072b6a6b80bf108c306193656
kevincp29 <josue_29c@hotmail.com>
2018-03-04 21:42:37 -0600

    Form Validation Add.

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8">
 	<title>CRUD</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<link REL=StyleSheet href="toast.css" type="text/css">
 </head>
 <body>
 	<div class="container">
@@ -23,21 +24,25 @@
 	        </button>
 	      </div>
 	      <div class="modal-body">
-	      	<form name="form1" method="post" action="insertarRegistro.php">
+	      	<form name="signInForm" method="post" action="insertarRegistro.php">
 			  <div class="form-group">
 			    <label>Name: </label>
-			    <input type="text" class="form-control" id="nombreUsuario" name="nombreUsuario" placeholder="Enter your name.">
+			    <input type="text" class="form-control" id="nombreUsuario" name="nombreUsuario" placeholder="Enter your name." required
+                data-fv-notempty-message="The name is required">
 			
 			  </div>
 			  <div class="form-group">
 			    <label>Age: </label>
-			    <input type="text" class="form-control" id="edadUsuario" name="edadUsuario" placeholder="Enter your age.">
+			    <input type="text" class="form-control" id="edadUsuario" name="edadUsuario" placeholder="Enter your age." required
+                data-fv-notempty-message="The age is required">
 			  </div>
 			  <div class="form-group">
 			    <label>Country: </label>
-			    <input type="text" class="form-control" id="paisUsuario" name="paisUsuario" placeholder="Enter your country.">
+			    <input type="text" class="form-control" id="paisUsuario" name="paisUsuario" placeholder="Enter your country." required
+                data-fv-notempty-message="The country is required">
 			  </div>
-			  <button type="submit" name="enviando" class="btn btn-primary">Submit</button>
+			  <button type="submit" name="enviando" class="btn btn-primary" >Submit</button>
+			  
 			</form> 	
 	      </div>
 	      <div class="modal-footer">
@@ -46,6 +51,8 @@
 	    </div>
 	  </div>
 	</div>
+	
+
 	<br>
 	<div class="container">
 		<table class="table table-dark">
@@ -68,13 +75,19 @@
 		 	  	echo "<td>".$res['Nombre']."</td>";
 		 	  	echo "<td>".$res['Edad']."</td>";
 		 	  	echo "<td>".$res['Pais']."</td>";
-		 	  	echo "<td><a href=\"editarRegistro.php?id=$res[usuarioId]\">Edit</a> | <a href=\"delete.php?id=$res[usuarioId]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";
+		 	  	echo "<td><button href=\"editarRegistro.php?id=$res[usuarioId]\">Edit</button> | <button href=\"delete.php?id=$res[usuarioId]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</button></td>";
 
 		 	  }
 			?>
 			</table>	
 	
 
+
+	<script type="text/javascript">
+		$(document).ready(function() {
+    		$('#signInForm').formValidation();
+		}); 
+	</script>
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" ></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" ></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" ></script>
--- insertarRegistro.php
+++ insertarRegistro.php
@@ -4,17 +4,15 @@
 	$nombreU=$_POST["nombreUsuario"];
 	$edadU=$_POST["edadUsuario"];
 	$paisU=$_POST["paisUsuario"];
+
 	
-	$query="CALL AddUser('$nombreU', $edadU, '$paisU')";
+		$query="CALL AddUser('$nombreU', $edadU, '$paisU')";
 
-	$resultado=mysqli_query($conexion, $query);
+		$resultado=mysqli_query($conexion, $query);
 
-	if($resultado == false){
-		echo "Error en la consulta";
-	} else {
-		echo "Se ha registrado los datos correctamente.";
-		header ("Location: index.php");
-	}
-
-
+		if($resultado == false){
+			echo "Error en la consulta";
+		} else {
+			header ("Location: index.php");
+		}
 ?>
